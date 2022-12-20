8bed28628f7c7c272e76f1b5c667384961f6f12d
joannaberlin <joanna.kot.ok@gmail.com>
2022-12-20 15:36:00 +0100

    Add link to readme

--- src/components/pages/Table.js
+++ src/components/pages/Table.js
@@ -1,21 +1,13 @@
 import { getTableById, getAllTables } from '../../redux/tablesRedux';
 import { useSelector } from 'react-redux';
 import { useParams, Navigate } from 'react-router-dom';
-// import { useEffect, useState } from 'react';
 import TableForm from '../features/TableForm';
 import Loading from '../common/Loading';
 
 const Table = () => {
 	const { tableId } = useParams();
 	const tableData = useSelector((state) => getTableById(state, tableId));
-	// const [hasData, setHasData] = useState(false);
 	const tables = useSelector((state) => getAllTables(state));
-
-	// useEffect(() => {
-	// 	if (tableData) {
-	// 		setHasData(true);
-	// 	}
-	// }, [tableData]);
 
 	if (!tableData) {
 		for (let table of tables) {
