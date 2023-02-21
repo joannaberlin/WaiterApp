98b4382264f7d65d2a0d4f645b2faf21c168d6de
joannaberlin <joanna.kot.ok@gmail.com>
2023-02-21 18:02:57 +0100

    Fix bug in about text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,9 @@
 const About = () => {
 	return (
 		<div className=''>
-			<p className='py-4 fs-2 my-5 '>
+			<p className='py-4 fs-3 my-5 '>
 				Waiter.app is here to make waiters' lifes easier by helping them to
-				manage tables on a daily shift'
+				manage tables on a daily shift.
 			</p>
 		</div>
 	);
