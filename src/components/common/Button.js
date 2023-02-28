251c5a1f624bf1ee1a4705c5fdd483879b723624
Joanna Kot <84131901+joannaberlin@users.noreply.github.com>
2023-02-28 12:40:08 +0000

    Styling changed and bugs fixed.

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,5 +1,5 @@
 const Button = (props) => {
-	return <button className='btn btn-dark my-2 mx-2'>{props.children}</button>;
+	return <button className='btn btn-info my-2 mx-2'>{props.children}</button>;
 };
 
 export default Button;
