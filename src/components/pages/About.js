dcfbeeb8620eb591492d6fc8810a8c0b5fd343c4
joannaberlin <joanna.kot.ok@gmail.com>
2023-02-28 16:44:50 +0100

    Test

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ const About = () => {
 		<div className=''>
 			<p className='py-4 fs-3 my-5 '>
 				Waiter.app is here to make waiters' lives easier by helping them to
-				manage tables during their shift.
+				manage tables during their daily shift.
 			</p>
 		</div>
 	);
