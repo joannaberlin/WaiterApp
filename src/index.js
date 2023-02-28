0766f5f15590dafbbfe573906fcaf617a412e9e3
joannaberlin <joanna.kot.ok@gmail.com>
2023-02-28 14:32:37 +0100

    Change styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import './styles/global.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
